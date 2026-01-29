8e003ee5d751df8693a52e62e101a182592cf0f1
santiago <sestevez5@gmail.com>
2026-01-29 13:33:24 +0000

    Modelando el servicio...

--- src/app/componentes/gestiones/alumnado/gestion-matriculas/gestion-matriculas.ts
+++ src/app/componentes/gestiones/alumnado/gestion-matriculas/gestion-matriculas.ts
@@ -17,7 +17,7 @@ export class GestionMatriculas {
   private readonly miServicio = inject(AlumnadoService);
   public alumnado = toSignal(this.miServicio.getTodos(), { initialValue: [] });
 
-  public x: any = this.miServicio.obtenerToken('{"apiKey":"2847789123741274967497812"}').subscribe(x => console.log(x))
+  public x: any = this.miServicio.obtenerToken("12D62B94-65CF-42E1-8E52-94EDE131E483").subscribe(x => console.log('Valor de x: ', x))
 
 
   readonly store = inject(GestionAlumnadoStore);
--- src/app/servicios/alumnado-service.ts
+++ src/app/servicios/alumnado-service.ts
@@ -1,21 +1,45 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { inject, Injectable, Signal, signal } from '@angular/core';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AlumnadoService {
+
+  SERVIDOR: string = "https://wwwpre.educacion.org/educacion/bussed/apieduca/v1";
+  APIKEY: string = "12D62B94-65CF-42E1-8E52-94EDE131E483";
+  token = signal<string | null>(null);
+
+
+
   private readonly http: HttpClient = inject(HttpClient);
 
-  token: string | null = null;
+
 
   public obtenerToken(apiKey: string): Observable<any> {
-    return this.http.post('https://wwwpre.educacion.org/educacion/bussed/apieduca/v1/seguridad/autenticacion-por-aplicacion', { apiKey: "12D62B94-65CF-42E1-8E52-94EDE131E483" })
+
+    const url: string = 'https://wwwpre.educacion.org/educacion/bussed/apieduca/v1/seguridad/autenticacion-por-aplicacion';
+
+    const body: IBodySolicitudToken = {
+      apiKey: apiKey
+    }
+
+    return this.http.post(url, body).pipe(
+      map((res: any) => res["access_token"])
+    )
   }
+
+
+
+
 
   public getTodos(): Observable<any[]> {
     return this.http.get<any[]>('https://jsonplaceholder.typicode.com/todos?_limit=10')
   }
 
+}
+
+interface IBodySolicitudToken {
+  apiKey: String;
 }
