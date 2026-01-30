a22259867d0fe076c48161e943d143a2fc4be451
sestevez5 <sestevez5@gmail.com>
2026-01-30 10:27:18 +0000

    cambios en el servicio.

--- src/app/componentes/gestiones/alumnado/gestion-matriculas/gestion-matriculas.ts
+++ src/app/componentes/gestiones/alumnado/gestion-matriculas/gestion-matriculas.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject, Signal } from '@angular/core';
 import { GestionAlumnadoStore } from '../../../../estados/gestionAlumnadoState'
 import { JsonPipe } from '@angular/common';
 import { Observable } from 'rxjs';
@@ -17,10 +17,19 @@ export class GestionMatriculas {
   private readonly miServicio = inject(AlumnadoService);
   public alumnado = toSignal(this.miServicio.getTodos(), { initialValue: [] });
 
-  public x: any = this.miServicio.obtenerToken("12D62B94-65CF-42E1-8E52-94EDE131E483").subscribe(x => console.log('Valor de x: ', x))
+
+
+    public valorToken: Signal<string | null> = this.miServicio.token;
+
+        public x: any = this.miServicio.obtenerToken("12D62B94-65CF-42E1-8E52-94EDE131E483");
 
 
   readonly store = inject(GestionAlumnadoStore);
+
+  constructor() {
+    effect( () => { console.log('valor token:'+ this.valorToken())})
+  }
+
 
 
 }
